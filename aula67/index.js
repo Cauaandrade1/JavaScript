77759463734ccb6453d803d12d84d74be86d6deb
Cauã de Andrade Campos <138033239+Cauaandrade1@users.noreply.github.com>
2024-04-02 23:27:04 -0300

    Aprendendo reduce em array

---
+++ aula67/index.js
@@ -0,0 +1,27 @@
+//some todos os números
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+
+const total = numeros.reduce(function(acumulador, valor){
+    acumulador += valor
+    return acumulador
+}, 0)
+
+console.log(total)
+
+
+//retorne a pessoa mais velha
+const pessoas = [
+    {nome: 'Luiz', idade: 62},
+    {nome: 'Maria', idade: 23},
+    {nome: 'Eduardo', idade: 55},
+    {nome: 'Letícia', idade: 19},
+    {nome: 'Rosana', idade: 72},
+    {nome: 'Wallace', idade: 47}
+]
+
+const velha = pessoas.reduce(function(acumulador,valor){
+    if(acumulador.idade > valor.idade) return acumulador
+        return valor
+})
+
+console.log(velha)
